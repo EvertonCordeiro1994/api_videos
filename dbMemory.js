1e5cc4a4ecd3aba352b7e1b08abba9b29994fb21
Everton Cordeiro <evertoncordeiro@icloud.com>
2024-08-10 17:32:46 -0300

    final do uso do db na memoria

--- dbMemory.js
+++ dbMemory.js
@@ -3,8 +3,25 @@ import { randomUUID } from "crypto"
 export class dataBaseMemory {
     #videos = new Map()
 
-    list(){
-        return this.#videos.values() 
+    list(search){
+        return Array.from(this.#videos.entries())
+        .map((videoArray) =>{ 
+            const id = videoArray[0]
+            const data = videoArray[1]
+
+            return{
+                id,
+                ... data
+            }
+        }) 
+
+        .filter(video => {
+            if (search) {
+                return video.title.includes(search)
+            }
+            return true
+        })
+
     }
     
     create(video){
--- server.js
+++ server.js
@@ -1,28 +1,46 @@
-import { fastify } from 'fastify'
+import fastify from 'fastify'
 import { dataBaseMemory } from './dbMemory.js'
 
 const server = fastify()
 const db = new dataBaseMemory()
 
+// Criar (Create)
 server.post('/videos', (request, reply) => {
-    const { title,description,duration} = request.body
-
-    db.create({
-        title,
-        description,
-        duration,
-    })
-
+    const { title, description, duration } = request.body
+    
+    db.create({ title, description, duration })
+    
     console.log(db.list())
-
-    return reply.status(201).send()
-
-
+    
+    return reply.status(201).send({ message: 'Vídeo criado com sucesso!' })
 })       
-server.get('/videos', () => 'rota 1')        
-server.put('/videos/:id', () => 'rota 1')    
-server.delete('/videos/:id', () => 'rota 1') 
 
+// Ler (Read)
+server.get('/videos', (request, reply) => {
+    const search = request.query.search
+    const videos = db.list(search)  
 
+    console.log(search)
+    return reply.send(videos)
+})
+
+// Atualizar (Update)
+server.put('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+    const { title, description, duration } = request.body
+    
+    db.update(videoId, { title, description, duration })
+    
+    return reply.status(200).send({ message: 'Vídeo atualizado com sucesso!' })
+})
+
+// Deletar (Delete)
+server.delete('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+
+    db.delete(videoId)
+
+    return reply.status(200).send({ message: 'Vídeo deletado com sucesso!' })
+}) 
 
 server.listen({ port: 3333 })
